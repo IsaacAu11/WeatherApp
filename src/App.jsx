b8edc2b50e3c3b9435117bc721b0d71486be1a76
IsaacAu11 <iszac987@gmail.com>
2024-09-10 23:56:51 +0100

    Added the main div and the card css

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 function App() {
 
   return (
-    <div className='overflow-auto'>
-      <div className='border-2 border-blue-400'>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-indigo-600">
+      <div className="max-w-lg w-full bg-white p-8 rounded-lg shadow-lg border-2 border-blue-400">
         <h1>Weather app</h1>
         <p className='text-10xl font-bold text-red-300'>Welcome to this simple weather app</p>
         <div>
