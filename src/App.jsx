885e43808f0c93322b4bfac0a67d5e299a021cda
IsaacAu11 <iszac987@gmail.com>
2024-09-10 23:53:05 +0100

    Created the base html

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='overflow-auto'>
+      <div className='border-2 border-blue-400'>
+        <h1>Weather app</h1>
+        <p className='text-10xl font-bold text-red-300'>Welcome to this simple weather app</p>
+        <div>
+          <input type="text" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
