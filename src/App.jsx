32894802494396fb27e61062381a2de055f43203
IsaacAu11 <iszac987@gmail.com>
2024-09-12 13:54:08 +0100

    transition between weather and start page under parent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import StartPage from "./components/startPage"
+import WeatherPage from "./components/weatherPage"
+import { useState } from "react"
 
 function App() {
-  return(<StartPage/>)
+  const[showWeatherPage,setShowWeatherPage] = useState(false)
+
+function updateWeatherPage(){
+  setShowWeatherPage(!showWeatherPage)
+}
+
+  return(
+    <div className='min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-indigo-600'>
+      {showWeatherPage ? <WeatherPage/> : <StartPage updateWeatherPage={updateWeatherPage}/>}
+    </div>
+  )
 }
 
 export default App
--- src/components/weatherPage.jsx
+++ src/components/weatherPage.jsx
@@ -2,7 +2,6 @@ export default function WeatherPage(){
     return(
         <>
             <p>this is the weather page!!!</p>
-            console.log("this is the page");
         </>
     )
 }
