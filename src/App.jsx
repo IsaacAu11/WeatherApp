bfa46bffb7b9e2f3c65bf30b1332f6603d1a3ad8
IsaacAu11 <iszac987@gmail.com>
2024-09-12 12:57:00 +0100

    set the on button click to send me to a new weather page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
-import './App.css'
-import './index.css'
-import searchIcon from '/Images/search.png'
-import weatherImage from '/Images/clouds-and-sun.png'
-
+import StartPage from "./components/startPage"
 
 function App() {
-
-  return (
-    <div className='min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-indigo-600'>
-      <div className='max-w-md w-full bg-white p-8 rounded-lg shadow-lg border-2 border-blue-400'>
-        <div className='flex items-center flex-col'>
-          <img src={weatherImage} alt='Cloud' className='w-60 h-60'></img>
-          <p className='mb-5 font-inria text-gray-500'>Welcome to Simple Weather</p>
-          <div className='relative'>
-          <img src={searchIcon} alt='search' className='absolute left-2 top-1 w-5 h-5 z-10' />
-            <button
-              className='absolute left-1 top-0 w-8 h-7 z-20'
-              onClick={() => alert('Button Clicked')}>
-            </button>
-            <input type="text" placeholder='Enter your city' className='border-2 rounded-md pl-9 w-96 text-gray-500'/> 
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+  return(<StartPage/>)
 }
 
 export default App
---
+++ src/components/weatherPage.jsx
@@ -0,0 +1,8 @@
+export default function WeatherPage(){
+    return(
+        <>
+            <p>this is the weather page!!!</p>
+            console.log("this is the page");
+        </>
+    )
+}
