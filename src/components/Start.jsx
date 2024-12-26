f4d1ad7b2ac6ae242393fa822c074437d2838d89
IsaacAu11 <iszac987@gmail.com>
2024-12-26 21:20:46 +0000

    full reset

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -3,18 +3,8 @@ import searchIcon from "/Images/search.png";
 import weatherImage from "/Images/clouds-and-sun.png";
 import { useState } from "react";
 
-export default function StartPage({ updateWeatherPage, setCity }) {
-  const [val, setVal] = useState("");
-  const [data, setData] = useState([]);
+export default function StartPage() {
 
-  const handleWeatherClick = () => {
-<<<<<<< HEAD
-    updateWeatherPage();
-=======
-    setCity(val); // Set the city state in the parent
-    updateWeatherPage(); // Update the weather page
->>>>>>> 6e37c459126f3ee570837e106b1c67ff2eec4c85
-  };
 
   return (
     <div className="max-w-md w-full bg-white p-8 rounded-lg shadow-lg border-2 border-blue-400">
@@ -31,57 +21,14 @@ export default function StartPage({ updateWeatherPage, setCity }) {
           />
           <button
             className="absolute left-1 top-0 w-8 h-7 z-20"
-<<<<<<< HEAD
-            onClick={() => getWeatherData(val)}
-=======
-            onClick={handleWeatherClick}
->>>>>>> 6e37c459126f3ee570837e106b1c67ff2eec4c85
           ></button>
           <input
             type="text"
             placeholder="Enter your city"
             className="border-2 rounded-md pl-9 w-96 text-gray-500"
-            value={val}
-            onChange={(e) => {
-              setVal(e.target.value);
-            }}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-<<<<<<< HEAD
-                getWeatherData(val);
-=======
-                handleWeatherClick(); // Call the function to set city and update weather page
->>>>>>> 6e37c459126f3ee570837e106b1c67ff2eec4c85
-              }
-            }}
           />
         </div>
       </div>
     </div>
   );
-<<<<<<< HEAD
-
-  function getWeatherData(city) {
-    const apiKey = `30f23130ada3d69eb69956b07a6fce2b`;
-    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&country=GB&limit=3&appid=${apiKey}`
-  
-    fetch(url)
-      .then( response =>{
-        console.log('Response: ',response);
-        response.json()
-      }
-      ).then(data => {
-        console.log('Data: ',data)
-        data => setData(data)
-      }
-      )
-    return(
-      <>
-
-      </>
-    )
-  }
 }
-=======
-}
->>>>>>> 6e37c459126f3ee570837e106b1c67ff2eec4c85
